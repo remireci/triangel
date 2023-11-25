678da1a85edd428d4754b9a172a7362c8060ce1d
remireci <reci.reciproque@gmail.com>
2023-11-25 10:50:40 +0100

    design_5

--- app/components/Welcome.js
+++ app/components/Welcome.js
@@ -10,7 +10,7 @@ const Welcome = () => {
           What is Lorem Ipsum?
         </h1>
         <p className="text-sm mb-6 text-black">
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry`&aposs standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#39;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
         </p>
         <Link
           href="/test"
