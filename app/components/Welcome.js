35cb1af4bd1fc8c41497acef31a1fabe98b8ce1e
remireci <reci.reciproque@gmail.com>
2024-02-21 21:50:11 +0100

    seo-14

--- app/components/Welcome.js
+++ app/components/Welcome.js
@@ -22,26 +22,6 @@ const Welcome = () => {
     }
   }, []);
 
-  // if (ip) {
-  //   console.log(ip);
-
-  //   return (
-  //     <div className="flex flex-row h-screen bg-[#cfe0e8] px-4 md:px-0 pt-28 md:pt-36 lg:pt-48">
-  //       <div className='w-0 md:w-1/4 lg:w-1/4'></div>
-  //       <div className='w-full md:w-1/2 lg:w-1/4 h-80 px-4 py-4 bg-[#daebe8] rounded shadow-md'>
-  //         <h1 className="text-xl mb-2 text-black">
-  //           Je hebt eerder reeds deelgenomen aan deze test.
-  //         </h1>
-  //         <p className="text-sm mb-2 text-black">
-  //           Het resultaat is je per mail toegestuurd. Kijk je spam folder na als je de mail niet gevonden hebt.
-  //           Contacteer ons om je resultaat met ons te bespreken. MAIL
-  //         </p>
-  //       </div>
-  //       <div className='w-0 md:w-1/4 lg:w-1/2'></div>
-  //     </div>
-  //   );
-  // };
-
 
   return (
     <div>
