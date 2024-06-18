8f54a6fe94c2f29e17b16803346c35f955a6d516
remireci <reci.reciproque@gmail.com>
2024-06-18 21:34:53 +0200

    resetting GA

--- app/components/GoogleAnalytics.tsx
+++ app/components/GoogleAnalytics.tsx
@@ -1,7 +1,7 @@
 "use client"
 import Script from "next/script";
 import { usePathname, useSearchParams } from 'next/navigation';
-import { useEffect, Suspense } from "react";
+import { useEffect } from "react";
 import { pageview } from "../lib/gtagHelper.ts";
 
 export default function GoogleAnalytics({ GA_MEASUREMENT_ID }: { GA_MEASUREMENT_ID: string }) {
@@ -17,13 +17,12 @@ export default function GoogleAnalytics({ GA_MEASUREMENT_ID }: { GA_MEASUREMENT_
     }, [pathname, searchParams, GA_MEASUREMENT_ID]);
 
     return (
-        <Suspense fallback={<div>Loading...</div>}>
-            <>
-                <Script strategy="afterInteractive"
-                    src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`} />
-                <Script id='google-analytics' strategy="afterInteractive"
-                    dangerouslySetInnerHTML={{
-                        __html: `
+        <>
+            <Script strategy="afterInteractive"
+                src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`} />
+            <Script id='google-analytics' strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                    __html: `
                 window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
@@ -36,9 +35,8 @@ export default function GoogleAnalytics({ GA_MEASUREMENT_ID }: { GA_MEASUREMENT_
                     page_path: window.location.pathname,
                 });
                 `,
-                    }}
-                />
-            </>
-        </Suspense>
+                }}
+            />
+        </>
     )
 }
