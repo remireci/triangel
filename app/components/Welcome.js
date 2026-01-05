0650812429b56a88fa6f333eaf0ad88486a4832f
reci.reciproque@gmail.com <reci.reciproque@gmail.com>
2026-01-05 15:13:00 +0100

    offline

--- app/components/Welcome.js
+++ app/components/Welcome.js
@@ -1,15 +1,14 @@
 // components/Welcome.js
-"use client"
-import React, { useState, useEffect } from 'react';
-import Link from 'next/link';
+"use client";
+import React, { useState, useEffect } from "react";
+import Link from "next/link";
 
 const Welcome = () => {
   const [ip, setIp] = useState("");
 
   useEffect(() => {
-
     const fetchIP = async () => {
-      const response = await fetch('/api/get-ip');
+      const response = await fetch("/api/get-ip");
       const data = await response.json();
       setIp(data);
     };
@@ -19,41 +18,54 @@ const Welcome = () => {
     }
   }, []);
 
-
   return (
     <div>
-      <div className='flex flex-row justify-center lg:mt-44 bg-[#102e4d]'>
-        <div className='w-0 md:w-1/5 lg:w-1/5'></div>
-        <div className='flex flex-col justify-between max-w-lg md:w-3/5 lg:w-3/5 h-86 mt-4 lg:mt-6 px-6 md:px-4 py-6 text-slate-300 text-base rounded shadow'>
-          <h2 className="mb-3">
-            Zit je op een kruispunt in je loopbaan? Haal je niet genoeg energie meer uit je job? Wordt de stress je te veel?
-            Is het tijd voor iets anders? Maar wat dan? Weet je niet goed wat je mogelijkheden zijn op de arbeidsmarkt?
+      <div className="flex flex-row justify-center lg:mt-44 bg-[#102e4d]">
+        <div className="w-0 md:w-1/5 lg:w-1/5"></div>
+        <div className="flex flex-col justify-between max-w-lg md:w-3/5 lg:w-3/5 h-86 mt-4 lg:mt-6 px-6 md:px-4 py-6 text-slate-300 text-base rounded shadow">
+          <h2 className="text-xl mb-3">
+            {/* Zit je op een kruispunt in je loopbaan? Haal je niet genoeg energie
+            meer uit je job? Wordt de stress je te veel? Is het tijd voor iets
+            anders? Maar wat dan? Weet je niet goed wat je mogelijkheden zijn op
+            de arbeidsmarkt? */}
+            Deze website is niet langer actief.
           </h2>
-          <h2 className="mb-3">
-            Kortom, zit je met vragen over je job en je loopbaan, doe de gratis loopbaantest en je krijgt vrijblijvend loopbaanadvies.
-            De test bestaat uit 18 eenvoudige meerkeuze vragen en duurt 5 minuten.
-          </h2>
-          <div className='flex flex-col mt-12 items-center'>
-            <Link
-              href="/test"
-            >
-              <p className="w-30 bg-[#87bdd8] hover:bg-blue-800 text-sm text-slate-200 uppercase rounded px-4 py-2">Start Test</p>
-            </Link>
-            <p className='text-xs text-slate-400 italic mt-8 text-center'>Bij deelname aan de test worden sommige van
-              je gegevens verwerkt. Meer info vind je <u><a href="/privacy" target="_blank" rel="noopener noreferrer">hier</a></u>.</p>
+          {/* <h2 className="mb-3">
+            Kortom, zit je met vragen over je job en je loopbaan, doe de gratis
+            loopbaantest en je krijgt vrijblijvend loopbaanadvies. De test
+            bestaat uit 18 eenvoudige meerkeuze vragen en duurt 5 minuten.
+          </h2> */}
+          <div className="flex flex-col mt-12 items-center">
+            {/* <Link href="/test">
+              <p className="w-30 bg-[#87bdd8] hover:bg-blue-800 text-sm text-slate-200 uppercase rounded px-4 py-2">
+                Start Test
+              </p>
+            </Link> */}
+            {/* <p className="text-xs text-slate-400 italic mt-8 text-center">
+              Bij deelname aan de test worden sommige van je gegevens verwerkt.
+              Meer info vind je{" "}
+              <u>
+                <a href="/privacy" target="_blank" rel="noopener noreferrer">
+                  hier
+                </a>
+              </u>
+              .
+            </p> */}
           </div>
         </div>
-        <div className='w-0 md:w-1/5 lg:w-1/5'></div>
+        <div className="w-0 md:w-1/5 lg:w-1/5"></div>
       </div>
       <div
-        style={{ backgroundImage: `url(/images/loopbaantest-job-ondersteuning.jpg)` }}
+        style={{
+          backgroundImage: `url(/images/loopbaantest-job-ondersteuning.jpg)`,
+        }}
         className="flex flex-row relative items-end justify-center h-[80vh] px-4 md:px-0 pt-28 md:pt-36 lg:pt-36 w-screen bg-center lg:bg-left-top bg-cover"
         alt="Vrijblijvend advies met de gratis loopbaantest."
       >
         <img
           src="/images/loopbaantest-job-ondersteuning.jpg"
           alt="loopbaanbegeleiding in heel Vlaanderen - loopbaantest jobcoach"
-          className="hidden"
+          className="blur-sm object-cover opacity-100 grayscale"
           aria-hidden="true"
         />
       </div>
